13456a33a5dd95d9c80fd7113b4e7750c7cc388e
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-09-23 15:49:36 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/piechartanychart/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/piechartanychart/MainActivity.java
@@ -1,14 +1,75 @@
 package com.rujirakongsomran.piechartanychart;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.anychart.AnyChart;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.chart.common.listener.Event;
+import com.anychart.chart.common.listener.ListenersInterface;
+import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
+import com.rujirakongsomran.piechartanychart.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        setupPieChart();
+    }
+
+    private void setupPieChart() {
+        Pie pie = AnyChart.pie();
+
+        pie.setOnClickListener(new ListenersInterface.OnClickListener() {
+            @Override
+            public void onClick(Event event) {
+                Toast.makeText(MainActivity.this,
+                        event.getData().get("x") + ":" + event.getData().get("value"), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("Apples", 6371664));
+        data.add(new ValueDataEntry("Pears", 789622));
+        data.add(new ValueDataEntry("Bananas", 7216301));
+        data.add(new ValueDataEntry("Grapes", 1486621));
+        data.add(new ValueDataEntry("Oranges", 1200000));
+
+        pie.data(data);
+        pie.title("Fruits imported in 2020 (in kg)");
+        pie.title().fontSize(18);
+        pie.labels().position("outside");
+
+
+        pie.legend().title().enabled(true);
+        pie.legend().title()
+                .text("Retail channels")
+                .padding(0d, 0d, 10d, 0d);
+
+        pie.legend()
+                .position("center-bottom")
+                .itemsLayout(LegendLayout.VERTICAL)
+                .align(Align.CENTER);
+
+        binding.anyChartView.setChart(pie);
+
     }
 }
